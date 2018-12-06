dc15b169d341b3ee5ba37f1b7bfeadb65e6c19fe
FroukeB <frouke.buyse@student.howest.be>
2018-12-07 00:44:42 +0100

    changes

--- project_bloem/project_bloem_spel/public/js/renderer.js
+++ project_bloem/project_bloem_spel/public/js/renderer.js
@@ -1,33 +1,22 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-const osc = require('osc');
-
-//  const udpPort = new osc.UDPPort({   remoteAddress: "localhost",   remotePort: 12345 }); // zenden van boodschap
-const udpPort = new osc.UDPPort({
-    localAddress: "localhost",
-    localPort: 12345
-}); // ontvangen van boodschap
-
-/* udpPort.on("ready", () => {
-    udpPort.send({
-        address: "/test",
-        args: [{           
-            type: "i",
-             value: 100
-            },{
-                type: "f",
-                value: 3.5
-            },{
-                type: "s",
-                value: "hello"
-            },{
-                type: "f",
-                 value: performance.now()
-            }
-        ]
+{   
+    const osc = require('osc');
+    const udpPort = new osc.UDPPort({
+        localAddress: "localhost",
+        localPort: 12345
     }); 
-}); */ // zenden boodschap
-udpPort.open();
-// ontvangen boodschap
-udpPort.on("message", (oscMsg) => {   console.log("An OSC message just arrived!", oscMsg); });
+    
+    const init = () => {
+        udpPort.open();
+        udpPort.on("message", (oscMsg) => {
+            console.log("An OSC message just arrived!", oscMsg);
+        });
+    };
+    
+    if (oscMsg.address === '/mouse/position') {
+    }
+    
+    init();   
+}
--- project_bloem/project_bloem_spel/public/js/script.js
+++ project_bloem/project_bloem_spel/public/js/script.js
@@ -1,14 +1,3 @@
-//import {UDPPort} from 'osc';
-
-const osc = require(`osc`);
-
-const udpPort = new osc.UDPPort({
-  localAddress: `localhost`,
-  localPort: 12345
-});
-
-
-
 const gif1Button = document.querySelector(`#gif1Button`);
 const gif1ButtonShow = document.querySelector(`#gif1ButtonShow`);
 const gif1 = document.querySelector(`gif1`);
@@ -26,14 +15,7 @@ const clickToShow = e => {
 
 
 const init = () => {
-  udpPort.open();
-  udpPort.on(`message`, oscMsg => {
-    console.log(oscMsg);
-  });
-
   gif1Button.addEventListener(`click`, e => clickOn(e));
   gif1ButtonShow.addEventListener(`click`, e => clickToShow(e));
-
-  
 };
 init();
